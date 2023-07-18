85d841ccd052d1cc935729b1da131221fe7eac4b
CHex0K <116585490+CHex0K@users.noreply.github.com>
2023-07-18 10:34:25 +0300

    Update __init__.py

--- doc2text/__init__.py
+++ doc2text/__init__.py
@@ -1,5 +1,11 @@
-from shiftlab_ocr.doc2text.reader import Reader
+'''from shiftlab_ocr.doc2text.reader import Reader
 
 __all__ = [
     'Reader'
 ]
+'''
+from . import utils
+from . import models
+from . import reader
+from . import recognition
+from . import segmintation
